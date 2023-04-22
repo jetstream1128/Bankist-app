ac3e414e91cb702d8c0933cb510fe1e7fb6ef5c5
jetstream1128 <bogdanalexeev2007@live.com>
2023-04-21 20:21:29 -0600

    a

--- script.js
+++ script.js
@@ -397,7 +397,7 @@ console.log(withdrawals);
 */
 //#endregion
 
-//#region ------Lecture 153 the reduce Method
+//#region ------Lecture 153 the reduce Method ------
 /*
 console.log(movements);
 
@@ -604,5 +604,46 @@ console.log(sortingMov);
 // });
 sortingMov = movements.sort((a, b) => b - a);
 console.log(sortingMov);
+*/
+//#endregion
+
+//#region ----- Lecture 164 More Ways of Creating and Filling Arrays -----
+/*
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7, 8));
+
+const x = new Array(7);
+console.log(x);
+// x.fill(1);
+console.log(x);
+// x.fill(1, 3);
+console.log(x);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+//Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// Challenge
+const diceRolls = Array.from(
+  { length: 100 },
+  cur => (cur = Math.floor(Math.random() * 6) + 1)
+);
+console.log(diceRolls);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
 */
 //#endregion
